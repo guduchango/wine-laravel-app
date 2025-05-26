e25d0547081fd40911829a1ac786c43a74b92315
Edgardo Ponce <gudu.chango@gmail.com>
2025-05-26 12:27:23 -0300

    create new wine controller, change auth controller, finish wine crud

---
+++ app/Http/Controllers/WineController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Wine;
+use Illuminate\Http\Request;
+
+class WineController extends Controller
+{
+    public function index()
+    {
+        return auth()->user()->wines;
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'winery' => 'required|string',
+            'variety' => 'required|string',
+            'vintage' => 'required|integer',
+            'country' => 'required|string',
+        ]);
+
+        $wine = auth()->user()
+            ->wines()
+            ->create($request->only(['name', 'winery', 'variety','vintage','country']));
+
+
+        return response()->json($wine, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $wine = auth()->user()->wines()->findOrFail($id);
+
+        $wine->update($request->only(['name', 'winery', 'variety','vintage','country']));
+
+        return response()->json($wine);
+    }
+
+    public function destroy($id)
+    {
+        $wine = auth()->user()->wines()->findOrFail($id);
+        $wine->delete();
+
+        return response()->json(['message' => 'Wine deleted']);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function wines()
+    {
+        return $this->hasMany(Wine::class);
+    }
 }
